27d00122081674d145e532f385a2b96627721a57
Simeon Duwel <tameimpalafan123@gmail.com>
2021-06-24 22:12:58 +0200

    Something changed. No idea what. Can't be bothered to check.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+// NOTORIO SOURCE CODE
+// Author: Simeon Duwel
+// This is most likely very bad, very inefficient code.
+// Please, by all means, open a pull request if you want to contribute to a poor man's software
+// projects. I'm not a good Rust dev, nor am I experienced. Ah well.
+
+use termion::color;
+use structopt::StructOpt;
+
+struct StyleOptions
+
+
 fn main() {
     println!("Hello, world!");
 }
